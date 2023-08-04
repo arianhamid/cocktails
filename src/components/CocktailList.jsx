d9a4e13a2c971cb19452c6842c5cfde40145ff6b
Hamid <hamidarianyegane@gmail.com>
2023-08-04 18:34:15 +0330

    cocktailslist updated

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { cocktails, isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No Cocktail Matches Your Criteria</h2>;
+  }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
-}
+    <section className="section">
+      <h2 className="section-title">Cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default CocktailList
+export default CocktailList;
--- src/context.jsx
+++ src/context.jsx
@@ -1,15 +1,57 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { useCallback } from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-const AppContext = React.createContext()
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      const { drinks } = data;
+
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+        setIsLoading(false);
+      } else {
+        setCocktails([]);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [searchTerm]);
+  return (
+    <AppContext.Provider
+      value={{ isLoading, cocktails, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
