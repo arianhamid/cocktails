94aec2cbc555df0f890e86c50d1a7ec696fc8295
Hamid <hamidarianyegane@gmail.com>
2023-08-04 13:19:12 +0330

    about and error pages updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae magni
+        itaque tempore quaerat minima, voluptatum unde voluptate fugiat fugit
+        esse libero iusto ipsam exercitationem. Dolorum soluta magnam nisi
+        sapiente corporis voluptate odit id cumque amet asperiores libero
+        veniam, aliquam deserunt ipsum vero expedita saepe quas quo inventore.
+        Voluptate, voluptatem totam?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
