d05cedbfd1a6f8f32882385da8e324bd1d383c87
Hamid <hamidarianyegane@gmail.com>
2023-08-04 19:02:17 +0330

    searchCocktail updated

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef("");
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form onSubmit={(e) => e.preventDefault()} className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            onChange={searchCocktail}
+            ref={searchValue}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
